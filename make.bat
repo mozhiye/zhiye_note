5c48798179ea989a26d7c43ee981a5bba6226b27
mozhiye <zhiyemo@outlook.com>
2020-07-27 23:27:30 +0800

    [dev]: 上传git.tex;

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @echo off                                                                                                                               
-set LSHORT=zhiye_note
+set LSHORT=git
 set PDF=%LSHORT%.pdf
 set TEMP=%LSHORT%.aux %LSHORT%.log
 
